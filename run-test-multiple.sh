d0306d10162a2bd193b722779b09b31fa71667f8
Jim Pick <jim@backblend.com>
2019-02-06 14:03:54 -0800

    Log test results

--- run-test-multiple.sh
+++ run-test-multiple.sh
@@ -6,35 +6,39 @@ if [ -z "$NUM" ]; then
   exit 1
 fi
 
-function finish {
-  echo "Cleaning up..."
-  kill $PID
-  wait $PID
-  rm $PIDFILE > /dev/null 2>&1 &
-  sleep 1
-}
+mkdir -p test/results
+OUTPUT=test/results/`date -u "+%Y%m%d_%H_%M_%S"`.txt
 
-#trap finish EXIT
-#trap "exit 2; finish" SIGINT
+(
+  function finish {
+    echo "Cleaning up..."
+    kill $PID
+    wait $PID
+    rm $PIDFILE > /dev/null 2>&1 &
+    sleep 1
+  }
 
-PIDFILE=/tmp/rendezvous-tracing.pid
+  #trap finish EXIT
+  #trap "exit 2; finish" SIGINT
 
-echo Starting Rendezvous
-if [ -f $PIDFILE ]; then
-  echo "Rendezvous server is already running, PID: $(< $PIDFILE)"
-  exit 1
-else
-  echo
-  #npx rendezvous &
-  #PID=$!
-  #echo $PID > $PIDFILE
-fi
+  PIDFILE=/tmp/rendezvous-tracing.pid
 
-for i in `seq 1 $NUM`; do
-  echo "Test Run $i of $NUM"
-  ./run-demo.sh
-  sleep 2
-  echo
-done
+  echo Starting Rendezvous
+  if [ -f $PIDFILE ]; then
+    echo "Rendezvous server is already running, PID: $(< $PIDFILE)"
+    exit 1
+  else
+    echo
+    #npx rendezvous &
+    #PID=$!
+    #echo $PID > $PIDFILE
+  fi
 
+  for i in `seq 1 $NUM`; do
+    echo "Test Run $i of $NUM"
+    ./run-demo.sh
+    sleep 2
+    echo
+  done
+) 2>&1 | tee $OUTPUT
 
